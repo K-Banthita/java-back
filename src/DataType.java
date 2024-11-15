39b1faabe68120b9f2a507717c87752161d51239
K-Banthita <bbnthita.054@gmail.com>
2024-11-15 12:20:42 +0700

    Add file

---
+++ src/DataType.java
@@ -0,0 +1,19 @@
+public static void main(String[] args) {
+//    variables use camelCase
+    int exampleOfInterger = 100;
+    long exampleOfLong = 100L;
+    byte exampleOfByte = 127;
+    short exampleOfShort = 100;
+    double exampleOfDouble = 25.00;
+    float exampleOfFloat = 25.00f;
+
+    char grade = 'D';
+    boolean isPass = true;
+
+    System.out.println("exampleOfByte: " + exampleOfByte);
+//    String template
+    System.out.println(STR."exampleOfByte: \{exampleOfByte}" );
+    System.out.println(STR."exampleOfLong: \{exampleOfLong}" );
+    System.out.println(STR."grade: \{grade}" );
+    System.out.println(STR."isPass: \{isPass}" );
+}
