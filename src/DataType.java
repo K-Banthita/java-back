e47fb409a7a692808169a852f2a17c838f43964c
K-Banthita <bbnthita.054@gmail.com>
2024-11-15 12:33:52 +0700

    Add file

--- src/DataType.java
+++ src/DataType.java
@@ -1,19 +1,22 @@
-public static void main(String[] args) {
-//    variables use camelCase
-    int exampleOfInterger = 100;
-    long exampleOfLong = 100L;
-    byte exampleOfByte = 127;
-    short exampleOfShort = 100;
-    double exampleOfDouble = 25.00;
-    float exampleOfFloat = 25.00f;
+public class DataType {
 
-    char grade = 'D';
-    boolean isPass = true;
+    public static void main(String[] args) {
+        //    variables use camelCase
+        int exampleOfInterger = 100;
+        long exampleOfLong = 100L;
+        byte exampleOfByte = 127;
+        short exampleOfShort = 100;
+        double exampleOfDouble = 25.00;
+        float exampleOfFloat = 25.00f;
 
-    System.out.println("exampleOfByte: " + exampleOfByte);
-//    String template
-    System.out.println(STR."exampleOfByte: \{exampleOfByte}" );
-    System.out.println(STR."exampleOfLong: \{exampleOfLong}" );
-    System.out.println(STR."grade: \{grade}" );
-    System.out.println(STR."isPass: \{isPass}" );
+        char grade = 'D';
+        boolean isPass = true;
+
+        System.out.println("exampleOfByte: " + exampleOfByte);
+        //    String template
+        System.out.println(STR."exampleOfByte: \{exampleOfByte}");
+        System.out.println(STR."exampleOfLong: \{exampleOfLong}");
+        System.out.println(STR."grade: \{grade}");
+        System.out.println(STR."isPass: \{isPass}");
+    }
 }
---
+++ src/Operator.java
@@ -0,0 +1,27 @@
+public class Operator {
+    public static void arithmetic() {
+
+    }
+    public static void relational() {
+
+    }
+    public static void logical() {
+
+    }
+    public static void bitwise() {
+
+    }
+    public static void assignment() {
+
+    }
+    public static void unary() {
+
+    }
+    public static void terrany() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
